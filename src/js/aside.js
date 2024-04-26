f58cf5ba4b1a6e2680c06f3d7ae0bcc8463f57dd
kmaltseva1 <eflceva78@gmail.com>
2024-04-26 22:05:14 +0700

    gyjhyg

--- src/js/aside.js
+++ src/js/aside.js
@@ -1,24 +1,31 @@
 //боковое меню
 const body = document.body
+const main = document.querySelector('.main')
 const burgerMenu = document.querySelector('.header__burger-btn')
 const burgerClose = document.querySelector('.aside__back-btn')
 const asideMenu = document.querySelector('.aside')
 const wrapperMenu = document.querySelector('.wrapper-menu')
+const bodyWrapper = document.querySelector('.body_wrapper')
 
 burgerMenu.addEventListener('click', function () {
-  asideMenu.classList.toggle('active')
-  asideMenu.classList.remove('close')
+  asideMenu.classList.toggle('aside--active')
+  asideMenu.classList.remove('aside--close')
+  main.classList.toggle('main--active')
 
-  wrapperMenu.classList.toggle('wrapper-menu--active')
-  wrapperMenu.classList.remove('close')
+  wrapperMenu.classList.remove('aside--close')
   body.classList.toggle('lock')
 })
+if (asideMenu.classList.contains('aside--active')) {
+  main.addEventListener('click', function (event) {
+    event.stopPropagation()
+  })
+}
 
 burgerClose.addEventListener('click', function () {
-  asideMenu.classList.remove('active')
-  asideMenu.classList.toggle('close')
+  asideMenu.classList.remove('aside--active')
+  main.classList.remove('main--active')
+  asideMenu.classList.toggle('aside--close')
 
-  wrapperMenu.classList.remove('wrapper-menu--active')
   body.classList.remove('lock')
 })
 
@@ -28,78 +35,78 @@ const burgerClose1 = document.querySelector('.btn-circle--close')
 const wrapperMenu1 = document.querySelector('.modal')
 
 burgerMenu1.addEventListener('click', function () {
-  wrapperMenu1.classList.toggle('actives')
-  wrapperMenu1.classList.remove('closes')
+  wrapperMenu1.classList.toggle('modal--active')
+  wrapperMenu1.classList.remove('modal--close')
+  main.classList.toggle('main--active')
 
-  wrapperMenu.classList.toggle('wrapper-menu--active')
   body.classList.add('lock')
 })
 
 burgerClose1.addEventListener('click', function () {
-  wrapperMenu1.classList.remove('actives')
-  wrapperMenu1.classList.toggle('closes')
+  wrapperMenu1.classList.remove('modal--active')
+  wrapperMenu1.classList.toggle('modal--close')
+  main.classList.remove('main--active')
 
-  wrapperMenu.classList.remove('wrapper-menu--active')
   body.classList.remove('lock')
 })
 
-//header-call
+//header-message
 const burgerMenu12 = document.querySelector('.header-call')
 const wrapperMenu12 = document.querySelector('.modal-call')
 const burgerClose12 = document.querySelector('.btn-circle--closes')
 
 burgerMenu12.addEventListener('click', function () {
-  wrapperMenu12.classList.toggle('actives')
-  wrapperMenu12.classList.remove('closes')
+  wrapperMenu12.classList.toggle('modal--active')
+  wrapperMenu12.classList.remove('modal--close')
+  main.classList.toggle('main--active')
 
-  wrapperMenu.classList.toggle('wrapper-menu--active')
   body.classList.add('lock')
 })
 
 burgerClose12.addEventListener('click', function () {
-  wrapperMenu12.classList.remove('actives')
-  // wrapperMenu12.classList.toggle('closes')
+  wrapperMenu12.classList.remove('modal--active')
+  main.classList.remove('main--active')
 
-  wrapperMenu.classList.remove('wrapper-menu--active')
   body.classList.remove('lock')
 })
 
 //aside-btn
 const burgerMenu2 = document.querySelector('.aside-chat')
+const wrapperActive = document.querySelector('.wrapper')
 
 burgerMenu2.addEventListener('click', function () {
-  wrapperMenu1.classList.toggle('actives')
-  wrapperMenu1.classList.remove('closes')
+  wrapperMenu1.classList.toggle('modal--active')
+  wrapperMenu1.classList.remove('modal--close')
+  wrapperActive.classList.toggle('wrapper--active')
 
-  asideMenu.classList.toggle('active')
+  asideMenu.classList.toggle('aside--active')
   body.classList.add('lock')
-  wrapperMenu.classList.add('wrapper-menu--active')
 })
 
 burgerClose1.addEventListener('click', function () {
-  wrapperMenu1.classList.remove('actives')
-  wrapperMenu1.classList.toggle('closes')
+  wrapperMenu1.classList.remove('modal--active')
+  wrapperMenu1.classList.toggle('modal--close')
+  wrapperActive.classList.remove('wrapper--active')
 
   body.classList.remove('lock')
-  wrapperMenu.classList.remove('wrapper-menu--active')
 })
 
-//aside-btn
+//aside-call
 const burgerMenu3 = document.querySelector('.aside-call')
 
 burgerMenu3.addEventListener('click', function () {
-  wrapperMenu12.classList.toggle('actives')
-  wrapperMenu12.classList.remove('closes')
+  wrapperMenu12.classList.toggle('modal--active')
+  wrapperMenu12.classList.remove('modal--close')
+  wrapperActive.classList.toggle('wrapper--active')
 
-  asideMenu.classList.toggle('active')
+  asideMenu.classList.toggle('aside--active')
   body.classList.add('lock')
-  wrapperMenu.classList.add('wrapper-menu--active')
 })
 
 burgerClose12.addEventListener('click', function () {
-  wrapperMenu12.classList.remove('actives')
-  wrapperMenu12.classList.toggle('closes')
+  wrapperMenu12.classList.remove('modal--active')
+  wrapperMenu12.classList.toggle('modal--close')
+  wrapperActive.classList.remove('wrapper--active')
 
   body.classList.remove('lock')
-  wrapperMenu.classList.remove('wrapper-menu--active')
 })
