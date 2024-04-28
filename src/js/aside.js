0ad7ab02b7f3ce1d82b3332ce6ccc00506765261
kmaltseva1 <eflceva78@gmail.com>
2024-04-29 04:52:04 +0700

    pr

--- src/js/aside.js
+++ src/js/aside.js
@@ -1,112 +1,113 @@
 //боковое меню
 const body = document.body
 const main = document.querySelector('.main')
-const burgerMenu = document.querySelector('.header__burger-btn')
+const burgerOpen = document.querySelector('.header__burger-btn')
 const burgerClose = document.querySelector('.aside__back-btn')
 const asideMenu = document.querySelector('.aside')
-const wrapperMenu = document.querySelector('.wrapper-menu')
-const bodyWrapper = document.querySelector('.body_wrapper')
 
-burgerMenu.addEventListener('click', function () {
-  asideMenu.classList.toggle('aside--active')
-  asideMenu.classList.remove('aside--close')
-  main.classList.toggle('main--active')
+burgerOpen.addEventListener('click', function (event) {
+  event.stopPropagation()
+  asideMenu.classList.add('aside--active')
 
-  wrapperMenu.classList.remove('aside--close')
-  body.classList.toggle('lock')
+  main.classList.add('main--active')
+  body.classList.add('lock')
 })
-if (asideMenu.classList.contains('aside--active')) {
-  main.addEventListener('click', function (event) {
-    event.stopPropagation()
-  })
-}
-
 burgerClose.addEventListener('click', function () {
   asideMenu.classList.remove('aside--active')
-  main.classList.remove('main--active')
-  asideMenu.classList.toggle('aside--close')
 
+  main.classList.remove('main--active')
   body.classList.remove('lock')
 })
 
-//header-message
-const burgerMenu1 = document.querySelector('.header-message')
-const burgerClose1 = document.querySelector('.btn-circle--close')
+//message
+const openMessage = document.querySelector('.header-message')
+const closeMessage = document.querySelector('.btn-circle--close')
 const wrapperMenu1 = document.querySelector('.modal')
 
-burgerMenu1.addEventListener('click', function () {
-  wrapperMenu1.classList.toggle('modal--active')
-  wrapperMenu1.classList.remove('modal--close')
-  main.classList.toggle('main--active')
+openMessage.addEventListener('click', function (event) {
+  event.stopPropagation()
+  wrapperMenu1.classList.add('modal--active')
 
+  main.classList.add('main--active')
   body.classList.add('lock')
 })
 
-burgerClose1.addEventListener('click', function () {
+closeMessage.addEventListener('click', function () {
   wrapperMenu1.classList.remove('modal--active')
-  wrapperMenu1.classList.toggle('modal--close')
   main.classList.remove('main--active')
 
   body.classList.remove('lock')
 })
 
-//header-message
-const burgerMenu12 = document.querySelector('.header-call')
-const wrapperMenu12 = document.querySelector('.modal-call')
-const burgerClose12 = document.querySelector('.btn-circle--closes')
+//call
+const openCall = document.querySelector('.header-call')
+const wrapperMenu2 = document.querySelector('.modal-call')
+const closeCall = document.querySelector('.btn-circle--closes')
 
-burgerMenu12.addEventListener('click', function () {
-  wrapperMenu12.classList.toggle('modal--active')
-  wrapperMenu12.classList.remove('modal--close')
-  main.classList.toggle('main--active')
+openCall.addEventListener('click', function (event) {
+  event.stopPropagation()
+  wrapperMenu2.classList.add('modal--active')
 
+  main.classList.add('main--active')
   body.classList.add('lock')
 })
 
-burgerClose12.addEventListener('click', function () {
-  wrapperMenu12.classList.remove('modal--active')
-  main.classList.remove('main--active')
+closeCall.addEventListener('click', function () {
+  wrapperMenu2.classList.remove('modal--active')
 
+  main.classList.remove('main--active')
   body.classList.remove('lock')
 })
 
-//aside-btn
-const burgerMenu2 = document.querySelector('.aside-chat')
+//aside-message
+const openAsideMessage = document.querySelector('.aside-chat')
 const wrapperActive = document.querySelector('.wrapper')
 
-burgerMenu2.addEventListener('click', function () {
-  wrapperMenu1.classList.toggle('modal--active')
-  wrapperMenu1.classList.remove('modal--close')
-  wrapperActive.classList.toggle('wrapper--active')
+openAsideMessage.addEventListener('click', function (event) {
+  event.stopPropagation()
+  wrapperMenu1.classList.add('modal--active')
+  wrapperActive.classList.add('wrapper--active')
+  main.classList.remove('main--active')
 
-  asideMenu.classList.toggle('aside--active')
+  asideMenu.classList.remove('aside--active')
   body.classList.add('lock')
 })
 
-burgerClose1.addEventListener('click', function () {
+closeMessage.addEventListener('click', function () {
   wrapperMenu1.classList.remove('modal--active')
-  wrapperMenu1.classList.toggle('modal--close')
   wrapperActive.classList.remove('wrapper--active')
 
   body.classList.remove('lock')
 })
 
 //aside-call
-const burgerMenu3 = document.querySelector('.aside-call')
+const openAsideCall = document.querySelector('.aside-call')
 
-burgerMenu3.addEventListener('click', function () {
-  wrapperMenu12.classList.toggle('modal--active')
-  wrapperMenu12.classList.remove('modal--close')
-  wrapperActive.classList.toggle('wrapper--active')
+openAsideCall.addEventListener('click', function (event) {
+  event.stopPropagation()
+  wrapperMenu2.classList.add('modal--active')
+  wrapperActive.classList.add('wrapper--active')
+  main.classList.remove('main--active')
 
-  asideMenu.classList.toggle('aside--active')
+  asideMenu.classList.remove('aside--active')
   body.classList.add('lock')
 })
 
-burgerClose12.addEventListener('click', function () {
-  wrapperMenu12.classList.remove('modal--active')
-  wrapperMenu12.classList.toggle('modal--close')
+closeCall.addEventListener('click', function () {
+  wrapperMenu2.classList.remove('modal--active')
   wrapperActive.classList.remove('wrapper--active')
 
   body.classList.remove('lock')
 })
+
+main.addEventListener('click', closeAll)
+
+function closeAll() {
+  asideMenu.classList.remove('aside--active')
+  wrapperMenu1.classList.remove('modal--active')
+  main.classList.remove('main--active')
+  wrapperActive.classList.remove('wrapper--active')
+
+  body.classList.remove('lock')
+  wrapperMenu2.classList.remove('modal--active')
+}
